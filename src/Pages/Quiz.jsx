513c391d9732a42d524211e46153881604e9ae3d
jenniferjoyce94 <jennifer.joyce.jaglund@gmail.com>
2025-09-19 09:35:06 +0200

    change in the quiz

--- src/Pages/Quiz.jsx
+++ src/Pages/Quiz.jsx
@@ -4,6 +4,17 @@ const Quiz = () => {
     <div>
       <Navbar />
       <h1>Quiz</h1>
+      <select name="countries" id="countries">
+        <option value="">Välj ett Region</option>
+        <option value="Africa">Afrika</option>
+        <option value="Americas">Amerika</option>
+        <option value="Asia">Asien</option>
+        <option value="Europe">Europa</option>
+        <option value="Oceania">Oceanien</option>
+      </select>
+      <input type="text" placeholder="Ditt namn" />
+      <button onClick={() => {}}>Starta Quiz</button>
+      {startGame === "playing" && <p>Quizet har startat!</p>}
     </div>
   );
 };
